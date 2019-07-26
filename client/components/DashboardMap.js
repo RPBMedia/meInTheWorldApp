1f1a8cf48ff5f8a3e4828278728b5b98bb616307
Rui Baiao <rui.baiao@tingcore.com>
2019-07-26 14:49:30 +0200

    tooltip bug fixed on the map

--- client/components/DashboardMap.js
+++ client/components/DashboardMap.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { VectorMap } from 'react-jvectormap';
 import { getCode, getData } from 'country-list';
 import { graphql } from 'react-apollo';
 import CurrentUserQuery from '../queries/CurrentUser';
 import Modal from './Modal';
 import { getCountryName } from '../utils'
+import Map from './Map';
 
 class DashboardMap extends Component {
   constructor(props) {
@@ -34,26 +34,27 @@ class DashboardMap extends Component {
 
   handleClick(e, countryCode) {
     e.preventDefault();
+    // eslint-disable-next-line react/no-string-refs
     console.log(countryCode);
     if(this.state.selectedCountry === null) {
       if(this.state.mapData.hasOwnProperty(countryCode)){
         this.setState({
-          // modalMessage: `${getCountryName(countryCode)}: ${this.state.mapData[countryCode]} locations visited`,
-          // modalShowing: true,
-          // selectedCountry: countryCode,
+          modalMessage: `${getCountryName(countryCode)}: ${this.state.mapData[countryCode]} locations visited`,
+          modalShowing: true,
+          selectedCountry: countryCode,
         })
       } else {
         this.setState({
-          // modalMessage: ` You haven't visited ${getCountryName(countryCode)} yet`,
-          // modalShowing: true,
-          // selectedCountry: countryCode,
+          modalMessage: ` You haven't visited ${getCountryName(countryCode)} yet`,
+          modalShowing: true,
+          selectedCountry: countryCode,
         })
       }
     } else {
       this.setState({
-        // modalMessage: null,
-        // modalShowing: null,
-        // selectedCountry: null,
+        modalMessage: null,
+        modalShowing: null,
+        selectedCountry: null,
       })
     }
   };
@@ -70,45 +71,15 @@ class DashboardMap extends Component {
 
     return (
       <div>
-        <VectorMap
-          map={"world_mill"}
-          backgroundColor="transparent"
-          zoomOnScroll={false}
-          containerStyle={{
-            width: "100%",
-            height: "520px"
-          }}
-          onRegionClick={(e, code) => this.handleClick(e, code)}
-          containerClassName="map"
-          regionStyle={{
-            initial: {
-              fill: "#e4e4e4",
-              "fill-opacity": 0.9,
-              stroke: "none",
-              "stroke-width": 0,
-              "stroke-opacity": 0
-            },
-            selected: {
-              fill: "#ff0000" //color for the clicked country
-            },
-            
-          }}
-          regionsSelectable={false}
-          series={{
-            regions: [
-              {
-                values: this.state.mapData, //this is your data
-                scale: ["#001100", "#00ff00"], //your color game's here, 
-                normalizeFunction: 'polynomial',
-              }
-            ]
-          }}
+        <Map
+          mapData={this.state.mapData}
+          handleClick={this.handleClick.bind(this)}
         />
-      {/* {this.state.modalShowing && 
+      {this.state.modalShowing && 
         <Modal
           message={this.state.modalMessage}
         />
-      } */}
+      }
       </div>
     );
   }
---
+++ client/components/Map.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { VectorMap } from 'react-jvectormap';
+
+class Map extends Component {
+
+  handleClickInitiated(e, code) {
+    // eslint-disable-next-line react/no-string-refs
+    this.refs.map.$mapObject.tip.hide();
+    this.props.handleClick(e, code);
+  }
+
+  render() {
+    return (
+      <div>
+        <VectorMap
+          map={"world_mill"}
+          // eslint-disable-next-line react/no-string-refs
+          ref={"map"}
+          backgroundColor="transparent"
+          zoomOnScroll={false}
+          containerStyle={{
+            width: "100%",
+            height: "520px"
+          }}
+          onRegionClick={(e, code) => this.handleClickInitiated(e, code)}
+          containerClassName="map"
+          regionStyle={{
+            initial: {
+              fill: "#e4e4e4",
+              "fill-opacity": 0.9,
+              stroke: "none",
+              "stroke-width": 0,
+              "stroke-opacity": 0
+            },
+            selected: {
+              fill: "#ff0000" //color for the clicked country
+            },
+            
+          }}
+          regionsSelectable={false}
+          labels={null}
+          series={{
+            regions: [
+              {
+                values: this.props.mapData, //this is your data
+                scale: ["#001100", "#00ff00"], //your color game's here, 
+                normalizeFunction: 'polynomial',
+              }
+            ]
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Map;
